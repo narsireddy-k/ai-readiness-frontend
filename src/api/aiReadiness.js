74dd6aa458a9666a2a4a8e0ee3dc56b61cec9a63
narsi-2208 <narsireddykasarla22@gmail.com>
2025-12-24 15:24:53 +0530

    Updated API endpoint

--- src/api/aiReadiness.js
+++ src/api/aiReadiness.js
@@ -1,24 +1,13 @@
-// import axios from "axios";
-
-// const API = axios.create({
-//   baseURL: "http://localhost:8000/api/ai-readiness/",
-// });
-
-// export const fetchQuestions = () => API.get("questions/");
-// export const submitAssessment = (payload) => API.post("submit/", payload);
-// export const fetchAssessment = (id) => API.get(`assessments/${id}/`);
-// export const downloadPDF = (id) =>
-//   window.open(`${API.defaults.baseURL}report/${id}/`, "_blank");
-
-
 import axios from "axios";
 
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
 const API = axios.create({
-  baseURL: "http://192.168.0.157:8000/api/ai-readiness/",
+  baseURL: `${API_BASE_URL}/api/ai-readiness/`,
 });
 
 export const fetchQuestions = () => API.get("questions/");
 export const submitAssessment = (payload) => API.post("submit/", payload);
 export const fetchAssessment = (id) => API.get(`assessments/${id}/`);
 export const downloadPDF = (id) =>
-  window.open(`${API.defaults.baseURL}report/${id}/`, "_blank");
+  window.open(`${API_BASE_URL}/api/ai-readiness/report/${id}/`, "_blank");
